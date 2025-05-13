439a549d59dd56d7bdad641e769104648aaf3dcd
kondo-akihiro-git <2000akisan@gmail.com>
2025-05-13 11:48:21 +0900

    issue発行テスト

--- sync_tasks.py
+++ sync_tasks.py
@@ -1,15 +1,21 @@
 import requests
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-# .envファイルを読み込む
-load_dotenv()
+# # .envファイルを読み込む
+# load_dotenv()
+
+# # GitHub API URL
+# API_URL = os.getenv("DOCUMENT_REPOS")
+
+# # GitHubトークン
+# GITHUB_TOKEN = os.getenv('API_TOKEN')
 
 # GitHub API URL
-API_URL = os.getenv("DOCUMENT_REPOS")
+API_URL = os.environ.get("DOCUMENT_REPOS")  # ← load_dotenv() を使わずに直接読む
 
 # GitHubトークン
-GITHUB_TOKEN = os.getenv('API_TOKEN')
+GITHUB_TOKEN = os.environ.get('API_TOKEN')
 
 # GitHub APIヘッダー
 headers = {
